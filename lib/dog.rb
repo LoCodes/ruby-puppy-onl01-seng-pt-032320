4fd3c2f1acc1c8a1a8031d512e792bbab097da6e
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-22 14:06:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,24 @@
 # Add your code here
 
+# Dog
+#   .new
+#     initializes with one argument - a name
+#   class variables
+#     @@all
+#       has a class variable, @@all, the points to an array
+#   class methods
+#     .all
+#       is a class method returns all dog instances
+#     .clear_all
+#       is a class method that empties the @@all array of all existing dogs
+#     .print_all
+#       is a class method that puts out the name of each dog to the terminal
+#   #save
+#     adds this dog instance to the @@all array when called
+#     gets called inside initialize when a new Dog is created
+#   #name
+#     has a name
+
 class Dog
   attr_accessor :name
 
@@ -19,7 +38,7 @@ class Dog
     #puts @@all.map{ |dog| dog.name } - easier way?
     @@all.each do |dog|
       puts dog.name
-    end 
+    end
   end
 
   def self.clear_all
